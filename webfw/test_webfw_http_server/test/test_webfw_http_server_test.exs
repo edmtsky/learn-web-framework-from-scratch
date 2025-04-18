0aed6684388f19aee47e9707ea925cfabf609f62
edmtsky <skythoren@gmail.com>
2025-04-18 06:22:11 +0300

    test_webfw_http_server add test

--- webfw/test_webfw_http_server/test/test_webfw_http_server_test.exs
+++ webfw/test_webfw_http_server/test/test_webfw_http_server_test.exs
@@ -2,7 +2,39 @@ defmodule TestWebfwHttpServerTest do
   use ExUnit.Case
   doctest TestWebfwHttpServer
 
-  test "greets the world" do
-    assert TestWebfwHttpServer.hello() == :world
+  setup_all do
+    Finch.start_link(name: Webfw.Finch)
+
+    :ok
+  end
+
+  # test "greets the world" do
+  #   assert TestWebfwHttpServer.hello() == :world
+  # end
+
+  describe "start/2" do
+    test "starts a server when responder is configured" do
+      Task.start_link(fn ->
+        Webfw.HttpServer.start(4041)
+      end)
+
+      {:ok, response} =
+        :get
+        |> Finch.build("http://localhost:4041/hello")
+        |> Finch.request(Webfw.Finch)
+
+      assert response.body == "Hello World\n"
+      assert response.status == 200
+      assert {"content-type", "text/html"} in response.headers
+
+      {:ok, response} =
+        :get
+        |> Finch.build("http://localhost:4041/bad")
+        |> Finch.request(Webfw.Finch)
+
+      assert response.body == "Not Found\n"
+      assert response.status == 404
+      assert {"content-type", "text/html"} in response.headers
+    end
   end
 end
