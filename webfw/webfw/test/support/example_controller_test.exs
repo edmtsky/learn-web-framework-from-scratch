c7f298fb3b4b6dfd0a1578b01dfbd6eb7e977783
edmtsky <skythoren@gmail.com>
2025-04-19 13:07:31 +0300

    webfw: add tests for a sample webapp (/greet & /redirect_greet) Plug.Test

--- webfw/webfw/test/support/example_controller_test.exs
+++ webfw/webfw/test/support/example_controller_test.exs
@@ -1,15 +1,30 @@
-defmodule Webfw.ExampleControllerTest do
+defmodule WebApp.ExampleControllerTest do
   use ExUnit.Case
-  use Plug.Test
+  import Plug.Test
+  # use Plug.Test
+  import Plug.Conn
 
   describe "GET /greet" do
     test "responds with 200 status" do
       conn = conn(:get, "/greet")
 
-      conn = Webfw.ExampleRouter.call(conn, [])
+      conn = WebApp.ExampleRouter.call(conn, [])
 
       assert conn.status == 200
       assert conn.resp_body == Jason.encode!(%{status: "ok"})
+    end
+  end
+
+  describe "GET /redirect_greet" do
+    test "responds with a redirect status" do
+      conn = conn(:get, "/redirect_greet")
+
+      conn = WebApp.ExampleRouter.call(conn, [])
+
+      assert conn.status == 302
+      assert conn.resp_body =~ "You are being"
+      assert conn.resp_body =~ "redirected"
+      assert Plug.Conn.get_resp_header(conn, "location") == ["/greet"]
     end
   end
 end
